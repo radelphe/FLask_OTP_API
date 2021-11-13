de8f90e1f4dbfce39e47011ea975b522d2221ed7
Gaurav Dubey <dubeygaurav0506@gmail.com>
2021-11-14 02:45:18 +0530

    edit in message

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
 import smtplib
-import math, random
+import math
+import random
+import os
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 from flask_cors import CORS
-import os 
-from flask import Flask,render_template, request, redirect, url_for,jsonify,session
 PORT = int(os.environ.get('PORT', 33507))
 HOST = 'https://otp--flask-api.herokuapp.com'
-#--------------------------------------------------------------------------------------------------------------
-sender_email='qliodev@gmail.com'         
-password='famousguy'
-#--------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------
+sender_email = 'qliodev@gmail.com'
+password = 'famousguy'
+# --------------------------------------------------------------------------------------------------------------
 # function to generate OTP
-def generateOTP() : 
+
+
+def generateOTP():
     digits = "0123456789"
     OTP = ""
-    for i in range(4) :
+    for i in range(4):
         OTP += digits[math.floor(random.random() * 10)]
     return OTP
-    
-#send mail    
-def mail(email,OTP):
-	global sender_email,password
-	s = smtplib.SMTP('smtp.gmail.com', 587)
-	s.starttls()
-	s.login(sender_email, password)
-	s.sendmail(sender_email,email, str(OTP))
-	s.quit()
+
+# send mail
+
+
+def mail(email, OTP):
+    global sender_email, password
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(sender_email, password)
+    s.sendmail(sender_email, email, 'Use the code: '+ str(OTP) + ' for logging into Queuelio' )
+    s.quit()
+
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/OTP', methods =["GET", "POST"])
+
+
+@app.route('/OTP', methods=["GET", "POST"])
 def home():
-    email_=request.args.get('id')
-    OTP_=generateOTP()
-    mail(email_,OTP_)
-    return jsonify(OTP=OTP_,email=email_)
-    
-    
-if __name__ == '__main__' :
-        app.run(debug=False,host = HOST, port = PORT)
+    email_ = request.args.get('id')
+    OTP_ = generateOTP()
+    mail(email_, OTP_)
+    return jsonify(OTP=OTP_, email=email_)
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host=HOST, port=PORT)
